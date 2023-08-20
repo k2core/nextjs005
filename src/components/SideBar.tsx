916b82bc74dfc8f715a54ea1c2440b5b3977ea54
k2core <7179062@gmail.com>
2023-08-20 23:05:19 +0900

    13.2-4 사이드바 구현(1) : 컴포넌트
    Changes to be committed:
    modified:   src/app/page.tsx
    new file:   src/components/FollowingBar.tsx
    new file:   src/components/PostList.tsx
    new file:   src/components/SideBar.tsx
    new file:   src/model/user.ts

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,23 @@
+import { User } from "@/model/user";
+import Avatar from "./Avatar";
+
+type Props = {
+  user: User;
+};
+
+export default function SideBar({ user: { name, username, image } }: Props) {
+  return (
+    <>
+      <div>
+        {image && <Avatar image={image} />}
+        <p>{username}</p>
+        <p>{name}</p>
+      </div>
+      <p>
+        About • Help • Press • API • Jobs • Privacy • Terms • Location •
+        Language
+      </p>
+      <p>@Copyright INSTANTGRAM from METAL</p>
+    </>
+  );
+}
