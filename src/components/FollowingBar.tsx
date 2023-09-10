69db68ad13b1052b196b6684c1e3e31e0d6b17ec
k2core <7179062@gmail.com>
2023-09-10 14:58:33 +0900

    팔로잉바 구현(3) : 컴포넌트
    TypeScript User 타입을 상세히 선언하고,
    로딩 스피너(react-spinners)를 설치하고,
    팔로잉바를 구현함.
    스타일링은 다음 장에서..
    
    Changes to be committed:
    modified:   package.json
    modified:   src/components/Avatar.tsx
    modified:   src/components/FollowingBar.tsx
    modified:   src/model/user.ts
    modified:   yarn.lock

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,8 +1,32 @@
 "use client";
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
 import useSWR from "swr";
+import Avatar from "./Avatar";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/me");
-  console.log("data ->", data);
-  return <p>FollowingBar</p>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
+  const users = data?.following;
+
+  return (
+    <section>
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul>
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link href={`/user/${username}`}>
+                <Avatar image={image} highlight />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
