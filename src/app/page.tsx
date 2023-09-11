88dba1135b4d4deb5b9bd2600972638d94363ebf
k2core <7179062@gmail.com>
2023-09-11 19:43:32 +0900

    팔로잉바 구현(4) : 스타일링
    Changes to be committed:
    modified:   src/app/page.tsx
    modified:   src/components/FollowingBar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function HomePage() {
 
   return (
     <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
-      <div className="w-full basis-3/4">
+      <div className="w-full basis-3/4 min-w-0">
         <FollowingBar />
         <PostList />
       </div>
